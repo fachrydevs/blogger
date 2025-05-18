9f232d157046ba7cb47f0eef589d4ff9ae2ce058
fachryrasya <fachrydevs@gmail.com>
2025-05-18 16:48:37 +0700

    fix bug

--- resources/views/components/Posts/post.blade.php
+++ resources/views/components/Posts/post.blade.php
@@ -11,19 +11,3 @@
         <p class="mt-4">Posted by <a href="#!">{{ $user }}</a> {{ $date }}</p>
     </div>
 </div>
-{{-- <div class="post-preview">
-   <a href="{{ $link }}">
-       <h2 class="post-title">{{ $title }}</h2>
-       <h3 class="post-subtitle">
-           @isset($description)
-               {{ $description }}
-           @endisset
-       </h3>
-   </a>
-   <p class="post-meta">
-       Posted by
-       <a href="#!">{{ $user }}</a>
-       {{ $date }}
-   </p>
-</div>
-<hr class="my-4" /> --}}
